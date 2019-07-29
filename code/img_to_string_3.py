294f5f8f577451f3939d4ac65683b6daa89814e9
Ashishfc <ashishs@freightcrate.in>
2019-07-29 11:05:21 +0530

    code </> Mon, 2019-07-29, 11:05:21 IST 210

--- code/img_to_string_3.py
+++ code/img_to_string_3.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pytesseract import image_to_string
+import re
 
 
 # print(image_to_string(Image.open('docs/imgs/address-fc.jpg')))
@@ -8,6 +9,10 @@ img_file = 'docs/imgs/address-fc_2.jpg'
 data_from_img = image_to_string(Image.open(img_file))
 
 outfile = "ocr_op/OCR-output_img-to-str_2.txt"
+f = open(outfile, "w")
+f.write('')
+f.close()
+
 f = open(outfile, "a")
 
 data_from_img = data_from_img.split()
@@ -16,11 +21,15 @@ append_start = False
 
 for words in data_from_img:
     print(words.lower())
+
     if 'address' == words.lower() and not append_start:
         globals()['append_start'] = True
 
     if globals()['append_start']:
         f.write(words.lower() + ' ')
+
+    if 6 == len(words) and re.findall(r'(\d{6})', words):
+        globals()['append_start'] = False
 
 f.close()
 
